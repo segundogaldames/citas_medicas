f7849768a5bf5031b9d33469c0c4ca217cee8c4a
segundogaldames <segundogaldames@gmail.com>
2024-06-01 11:07:11 -0400

    Implementacion de metodo create y store a controlador roles

--- controllers/rolesController.php
+++ controllers/rolesController.php
@@ -20,4 +20,44 @@ class rolesController extends Controller
         #select id, nombre from roles order by nombre
         $this->_view->load('roles/index', compact('options','msg_success','msg_error'));
     }
+
+    #metodo get para cargar un formulario de registro
+    public function create()
+    {
+        list($msg_succcess, $msg_error) = $this->getMessages();
+
+        $options = [
+            'title' => 'Roles',
+            'rol' => Session::get('data'),
+            'process' => 'roles/store',
+            'send' => $this->encrypt($this->getForm())
+        ];
+
+        $this->_view->load('roles/create', compact('options','msg_success','msg_error'));
+    }
+
+    #metodo post para gestionar el registro de los roles
+    public function store()
+    {
+        $this->validateForm('roles/create',[
+            'nombre' => Filter::getText('nombre')
+        ]);
+
+        #comprobar que el rol ingresado no exista
+        #select id from roles where nombre = ?
+        $rol = Role::select('id')->where('nombre', Filter::getText('nombre'))->first();
+
+        if ($rol) {
+            Session::set('msg_error', 'El rol ingresado ya existe... intenta con otro');
+            $this->redirect('roles/create');
+        }
+
+        $rol = new Role;
+        $rol->nombre = Filter::getText('nombre');
+        $rol->save();
+        
+        Session::destroy('data');
+        Session::set('msg_success','El rol se ha registrado correctamente');
+        $this->redirect('roles');
+    }
 }
